b7ffedb6e25d6a028acab917376f32913a619304
Pascal Frenz <5677521+PascalFrenz@users.noreply.github.com>
2024-03-17 23:20:14 +0100

    configure Astro to deploy from Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pascalfrenz.github.io',
+	base: '/blog',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
